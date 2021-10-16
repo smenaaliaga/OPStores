704e976b93d0911dc963d46dddaa0b0168038a78
Seba <smenaaliaga@gmail.com>
2021-10-16 18:50:40 -0300

    Border diagonal

--- lib/test/widgets/headers.dart
+++ lib/test/widgets/headers.dart
@@ -21,3 +21,44 @@ class HeaderRedondeado extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        // color: Color(0xff615AAB),
+        child: CustomPaint(
+          painter: _HeaderDiagonalPainter(),
+        ));
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // Propiedades
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;
+
+    final path = Path();
+
+    // Dibujar
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    // path.lineTo(0, size.height * 0.5);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
