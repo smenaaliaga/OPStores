98f940f970917f388c50f3011085663832b13f96
Seba <smenaaliaga@gmail.com>
2021-10-16 19:05:40 -0300

    Border Cuadratico y Cubico

--- lib/test/widgets/headers.dart
+++ lib/test/widgets/headers.dart
@@ -62,3 +62,125 @@ class _HeaderDiagonalPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderPico extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        // color: Color(0xff615AAB),
+        child: CustomPaint(
+          painter: _HeaderPicoPainter(),
+        ));
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // Propiedades
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;
+
+    final path = Path();
+
+    // Dibujar
+    path.moveTo(0, size.height * 0.25);
+    path.lineTo(size.width * 0.5, size.height * 0.3);
+    path.lineTo(size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    // path.lineTo(0, size.height * 0.5);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderCurvo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        // color: Color(0xff615AAB),
+        child: CustomPaint(
+          painter: _HeaderCurvoPainter(),
+        ));
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // Propiedades
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;
+
+    final path = Path();
+
+    // Dibujar
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height * 0.35, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderCubico extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        // color: Color(0xff615AAB),
+        child: CustomPaint(
+          painter: _HeaderCubicoPainter(),
+        ));
+  }
+}
+
+class _HeaderCubicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // Propiedades
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;
+
+    final path = Path();
+
+    // Dibujar
+    path.lineTo(0, size.height * 0.25);
+    path.cubicTo(size.width * 0.5, size.height * 0.35, size.width * 0.5,
+        size.height * 0.15, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
